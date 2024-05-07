3c0f4d072b282a461f11fe8e883a3e6880891578
shirvel <shirvel2000@gmail.com>
2024-05-07 19:54:34 +0300

    add the add comment page UI

--- app/src/main/java/com/example/app/Modules/Comments/AddComment.kt
+++ app/src/main/java/com/example/app/Modules/Comments/AddComment.kt
@@ -1,25 +1,28 @@
 package com.example.app.Modules.Comments
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.navigation.Navigation
-import com.example.app.model.PostListModel
-import com.example.app.model.Post
-import com.example.app.Modules.Posts.AllPosts
 import com.example.app.R
-import androidx.navigation.NavController
 
 
+class addComment : Fragment() {
 
 
-class AddComment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_comment, container, false)
+    }
 
 
 }
