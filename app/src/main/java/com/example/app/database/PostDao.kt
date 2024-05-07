bb5e63019024aff8fed3e36a7832f0e0cb0ff0e5
shelly-schwartzman <shellyshvartzman@gmail.com>
2024-05-07 20:33:41 +0300

    Added delete func

--- app/src/main/java/com/example/app/database/PostDao.kt
+++ app/src/main/java/com/example/app/database/PostDao.kt
@@ -17,7 +17,11 @@ interface PostDao {
     @Query("SELECT * FROM posts")
     fun getAllPosts(): LiveData<MutableList<Post>>
 
+    @Query("DELETE FROM posts WHERE postId = :postId")
+    fun deletePost(postId: String)
     @Delete
     fun delete(post: Post)
+    @Query("SELECT * FROM posts WHERE postId = :postId")
+    fun getPostById(postId: String): LiveData<Post>
 
 }
