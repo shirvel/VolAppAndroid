d0dbf33479c2c99cc472faacf0b211deaecb1b63
ortalGolzarP <ortalgolzar@gmail.com>
2024-05-08 12:37:26 +0300

    user-photos-not-working

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,8 @@ dependencies {
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    implementation ("com.google.firebase:firebase-storage-ktx:20.0.0")
+    implementation ("com.squareup.picasso:picasso:2.71828")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
