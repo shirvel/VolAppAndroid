165b11bd525353c54d0028da0a5becae0adcf194
ortalGolzarP <ortalgolzar@gmail.com>
2024-05-08 21:40:52 +0300

    move-settings-to-be-in-myApplication

--- app/src/main/java/com/example/app/base/MyApplication.kt
+++ app/src/main/java/com/example/app/base/MyApplication.kt
@@ -2,6 +2,11 @@ package com.example.app.base
 
 import android.app.Application
 import android.content.Context
+import com.google.firebase.firestore.firestoreSettings
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.memoryCacheSettings
+import com.google.firebase.ktx.Firebase
+
 class MyApplication: Application(){
     object Globals{
         var appContext: Context? = null
@@ -10,5 +15,10 @@ class MyApplication: Application(){
     override fun onCreate() {
         super.onCreate()
         Globals.appContext = applicationContext
+
+        val settings = firestoreSettings {
+            setLocalCacheSettings(memoryCacheSettings { })
+        }
+        Firebase.firestore.firestoreSettings = settings
     }
 }
